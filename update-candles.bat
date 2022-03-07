d3d3b6320f05189cc2e2c62304f6f2047c5d836d
dem2k <dem@deminator.de>
2022-03-07 19:16:13 +0100

    update start script.

--- update-candles.bat
+++ update-candles.bat
@@ -1,2 +1,8 @@
-@call %~dp0class-runner.bat dem2k.Main %*
-@if errorlevel 1 pause
+@echo off
+set CLASSPATH=%~dp0target\classes;%~dp0config
+for %%i in ("%~dp0target\dependency\*.jar") do call :addcp %%i
+java dem2k.Main %*
+goto ende
+:addcp
+set CLASSPATH=%1;%CLASSPATH%
+:ende
